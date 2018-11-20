1a0cbcb60c6a8e4bd9df99f5d24f37b08bc80251
luben karavelov <luben@amazon.com>
2018-11-20 18:09:59 +0000

    Add negative example - worker-non-stream does not cause leaks

---
+++ src/bin/worker-non-stream.rs
@@ -0,0 +1,67 @@
+use futures::future::Future;
+use futures::stream::{self, Stream};
+use hyper::{client::HttpConnector, rt, Body, Client, Request, Uri};
+
+fn main() -> std::result::Result<(), ()> {
+    let next = "http://127.0.0.1:9999/next".parse::<Uri>().unwrap();
+    let response = "http://127.0.0.1:9999/response".parse::<Uri>().unwrap();
+
+    let mut http = HttpConnector::new_with_tokio_threadpool_resolver();
+    http.set_nodelay(true);
+
+    // build client
+    let client_get = Client::builder()
+        .keep_alive(true)
+        .keep_alive_timeout(None)
+        //.http2_only(true)
+        .max_idle_per_host(1)
+        .build(http);
+    // use second client for the replies
+    let client_post = client_get.clone();
+
+    let process = stream::repeat::<(), ()>(())
+        .and_then(move |_| {
+            client_get
+                .request(
+                    Request::builder()
+                        .method("GET")
+                        .uri(next.clone())
+                        .body(Body::empty())
+                        .expect("Bad Request"),
+                )
+                .map_err(|e| println!("GET {:?}", e))
+        })
+        .and_then(move |req| {
+            let id = req
+                .headers()
+                .get("X-Request-Id")
+                .expect("RequestId missing")
+                .clone();
+            req.into_body()
+                .map(|chunk| {
+                    chunk
+                        .iter()
+                        .map(|byte| byte.to_ascii_uppercase())
+                        .collect::<Vec<u8>>()
+                })
+                .concat2()
+                .map(|body| (id, body))
+                .map_err(|e| println!("TRASFORM {:?}", e))
+        })
+        .and_then(move |(id, body)| {
+            client_post
+                .request(
+                    Request::builder()
+                        .method("POST")
+                        .header("X-Request-Id", id)
+                        .uri(response.clone())
+                        .body(body.into())
+                        .expect("Invalid request"),
+                )
+                .map_err(|e| println!("POST {:?}", e))
+        })
+        .for_each(|_| Ok(()))
+        .map_err(|_| ());
+
+    Ok(rt::run(process))
+}
